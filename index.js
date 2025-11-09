5b8b5adad851c57656aee593b28d829249217e78
Vivek <152777809+vivekfaujdar01@users.noreply.github.com>
2025-11-09 10:05:18 +0530

    Add dotenv for securing info

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env"
+});
+let myusername = process.env.username;
 console.log("Starting the backend server...");
+console.log("Username from .env file:", myusername);
