b27ed5e74fe4f7b1069ae618f0a69ab282e68705
Vivek <152777809+vivekfaujdar01@users.noreply.github.com>
2025-11-09 19:59:14 +0530

    connect to database using mongoose

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/index.js";
 dotenv.config({
     path: "./.env"
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
+//connect to database
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port http://localhost:${PORT}`);
+    });
+}).catch((err)=> {
+    console.error("Failed to connect to database:", err);
+    process.exit(1);
 });
-
