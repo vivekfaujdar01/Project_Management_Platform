f4371070af47464680ab29f9d9166260484f1b1e
Vivek <152777809+vivekfaujdar01@users.noreply.github.com>
2025-11-11 14:35:20 +0530

    adding async healthChecker

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -1,5 +1,13 @@
 import { ApiResponse } from "../utils/api-reponse.js";
+import { asyncHandler } from "../utils/async-handler.js";
 
+// Health Check Controller using asyncHandler
+const healthCheck = asyncHandler(async (req, res) => {
+    res.status(200).json(new ApiResponse(200, {message: "Server is running"}, "API is healthy"));
+});
+
+// Alternative without asyncHandler and without async/await
+/*
 const healthCheck = (req, res) => {
     try{
         res.status(200).json(new ApiResponse(200, {message: "Server is running"}, "API is healthy"));
@@ -7,5 +15,18 @@ const healthCheck = (req, res) => {
         return res.status(500).json(new ApiResponse(500, null, "Internal Server Error"));
     }
 };
+*/
+// Alternative without asyncHandler and async/await
+/*
+const healthCheck = async(req, res, next) => {
+    try{
+        const user = await getUserFromDB(); // Example async operation
+        res.status(200).json(new ApiResponse(200, {message: "Server is running"}, "API is healthy"));
+    }catch(err){
+        next(err);
+    }
+}
+*/
+
 
 export { healthCheck };
