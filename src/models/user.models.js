643f1790625582889272a9ee2747fbe4bf18a512
Vivek <152777809+vivekfaujdar01@users.noreply.github.com>
2025-11-11 16:12:17 +0530

    Writing code for generating access , refresh and temporary token

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,6 +1,7 @@
-import mongoose,{Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
-
+import jwt from "jsonwebtoken";
+import crypto from "crypto"
 // User Schema
 const userSchema = new Schema(
     {
@@ -57,22 +58,57 @@ const userSchema = new Schema(
         emailVerificationExpiry: {
             type: Date
         }
-    },{
-        timestamps: true
-    }
+    }, {
+    timestamps: true
+}
 );
 
 // Hash the password with preHook of mongoose
-userSchema.pre("save", async function(next){
-    if(!this.isModified("password")) return next()
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next()
 
     this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
 // using bcrypt checking password is correct
-userSchema.methods.isPasswordCorrect = async function(password){
+userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
+}
+
+// Generating access token
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+    )
+}
+
+// Generating refresh token 
+userSchema.methods.genereateRefreshToken = function(){
+    return jwt.sign(
+        {
+        _id: this._id,
+        email: this.email,
+        username: this.username
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+    )
+}
+
+// Generating Temporary tokens (these are used for verifying the user and password reset)
+userSchema.methods.generateTemporaryToken = function (){
+    const unHashedToken = crypto.randomBytes(20).toString("hex");
+    const hashedToken = crypto.createHash("sha256").update(unHashedToken).digest("hex")
+
+    const tokenExpiry = Date.now() + (20*60*1000) // 20 mins
+    return {unHashedToken, hashedToken, tokenExpiry}
 }
 
 
